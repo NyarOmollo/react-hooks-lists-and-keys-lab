9245d5921c5ca52836b1a905aa72b3b55571f519
Nyaromollo <omolloenter@gmail.com>
2023-05-09 17:34:05 +0300

    Completed Asiignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import ColorList from "./ColorList";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ColorList />
     </div>
   );
 }
